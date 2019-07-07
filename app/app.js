830de62d9bf6eab4ce5fc1e04be7868fa8a98816
Pansul Bhatt <pansul47@gmail.com>
2019-07-07 12:12:51 +0530

    Updated app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ import { readCsvFile } from './utils/utils';
 
 const app = express();
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 const startServer = () => {
   app.listen(PORT, () => {
